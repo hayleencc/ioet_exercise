688edb6bac3d1c9bcd45c70f45a6a1606145bbb7
Hayleen Carrillo Carrillo <66764846+hayleencc@users.noreply.github.com>
2023-02-01 17:40:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.controllers.employees_controller import *
 from src.controllers.coincidence_controller import *
 
 def main():
-    schedules = FileReader.read_file(".\example_files\employees_schedule.txt") 
+    schedules = FileReader.read_file("./example_files/employees_schedule.txt") 
     employees = EmployeesController.create_some_employees(EmployeesController, schedules) 
     CoincidenceController.search_coincidences_schedule(CoincidenceController, employees)
 
